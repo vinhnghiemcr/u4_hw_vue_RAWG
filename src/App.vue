f5f57fa9e048d9bfc3b84361ec175d3cec211d35
Andre Pato <anpato1994@gmail.com>
2021-06-24 06:25:47 -0400

    update starter code

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,16 @@
   <div id="app">
     <header>
       <!-- Nav Goes Here -->
-      <Nav />
     </header>
     <main>
       <!-- Router View Goes Here -->
-      <router-view />
     </main>
   </div>
 </template>
 
 <script>
-import Nav from './components/Nav'
 export default {
   name: 'App',
-  components: {
-    Nav
-  }
+  components: {}
 }
 </script>
--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import GameDetails from './pages/GameDetails'
 import ViewGames from './pages/ViewGames'
 import About from './pages/About'
+
 const routes = []
 
 export default new VueRouter({ routes, mode: 'history' })
